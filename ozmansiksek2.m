36e306991df01f56ffab1375c04a79f1d229e772
joshabox <50105934+joshabox@users.noreply.github.com>
2020-10-31 10:38:12 +0100

    Update ozmansiksek2.m

--- ozmansiksek2.m
+++ ozmansiksek2.m
@@ -308,7 +308,81 @@ reduce:=function(X,Xp,D);
 	return Dp;
 end function;
 
-//The below function was written by Josha Box
+//The below functions were written by Josha Box
+ModCrvQuot:=function(N,wlist,remwlist)
+C:=CuspForms(N);
+ws:=[AtkinLehnerOperator(C,n) : n in wlist];
+remws:=[AtkinLehnerOperator(C,n) : n in remwlist];
+NN:=&meet[Nullspace(Matrix(w-1)) : w in ws];
+dim:=Dimension(NN);
+seqs:=[[Coordinates(NN,Basis(NN)[i]*Matrix(w)) : i in [1..dim]] : w in remws];
+BB:=[&+[(Integers()!(2*Eltseq(Basis(NN)[i])[j]))*C.j : j in [1..Dimension(C)]] : i in [1..dim]];
+prec:=500;
+L<q>:=LaurentSeriesRing(Rationals(),prec);
+R<[x]>:=PolynomialRing(Rationals(),dim);
+Bexp:=[L!qExpansion(BB[i],prec) : i in [1..dim]];
+eqns:=[R | ];
+	d:=1;
+	tf:=false;
+	while tf eq false do
+		d:=d+1;
+		mons:=MonomialsOfDegree(R,d);
+		monsq:=[Evaluate(mon,Bexp) : mon in mons];
+		V:=VectorSpace(Rationals(),#mons);
+		W:=VectorSpace(Rationals(),prec-10);
+		h:=hom<V->W | [W![Coefficient(monsq[i],j) : j in [1..(prec-10)]] : i in [1..#mons]]>;
+		K:=Kernel(h);
+		eqns:=eqns cat [ &+[Eltseq(V!k)[j]*mons[j] : j in [1..#mons] ] : k in Basis(K)  ];
+        I:=Radical(ideal<R | eqns>);
+		X:=Scheme(ProjectiveSpace(R),I);
+		if Dimension(X) eq 1 then
+			if IsSingular(X) eq false then
+				X:=Curve(ProjectiveSpace(R),eqns);
+				if Genus(X) eq dim then
+					tf:=true;
+				end if;
+			end if;
+		end if;
+	end while;
+	eqns:=GroebnerBasis(ideal<R | eqns>); // Simplifying the equations.
+	tf:=true;
+	repeat
+		t:=#eqns;
+		tf:=(eqns[t] in ideal<R | eqns[1..(t-1)]>);
+		if tf then 
+			Exclude(~eqns,eqns[t]);
+		end if;
+	until tf eq false;
+	t:=0;
+	repeat
+		t:=t+1;
+		tf:=(eqns[t] in ideal<R | Exclude(eqns,eqns[t])>);	
+		if tf then
+			Exclude(~eqns,eqns[t]);
+			t:=0;
+		end if;
+	until tf eq false and t eq #eqns;
+	X:=Curve(ProjectiveSpace(R),eqns); // Our model for X_0(N) discovered via the canonical embedding.
+	assert Genus(X) eq dim;
+	assert IsSingular(X) eq false;
+    indexGam:=N*&*[Rationals() | 1+1/p : p in PrimeDivisors(N)];	
+	indexGam:=Integers()!indexGam; // Index of Gamma_0(N) in SL_2(Z)
+	for eqn in eqns do
+		eqnScaled:=LCM([Denominator(c) : c in Coefficients(eqn)])*eqn;
+		wt:=2*Degree(eqn); // Weight of eqn as a cuspform.
+		hecke:=Ceiling(indexGam*wt/12);  // Hecke=Sturm bound.
+										// See Stein's book, Thm 9.18.
+		Bexp1:=[qExpansion(BB[i],hecke+10) : i in [1..dim]]; // q-expansions
+                        // of basis for S 
+                        // up to precision hecke+10.
+		assert Valuation(Evaluate(eqnScaled,Bexp1)) gt hecke+1;
+	end for; // We have now checked the correctness of the equations for X. 
+seqlist:=[[&+[seq[i][j]*x[j] : j in [1..dim]] : i in [1..dim]] : seq in seqs];
+wmaplist:=[iso<X->X | seq,seq> : seq in seqlist];
+return X,wmaplist;
+end function;
+
+
 MapFromQuotient:=function(N,ws,n)
 tf,m:=IsSquare(Integers()!(N/n));
 assert tf;

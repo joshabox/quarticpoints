6ea5956a8ebd824201f06851627b59bdf83c90ab
joshabox <50105934+joshabox@users.noreply.github.com>
2021-01-24 10:43:47 +0100

    Update QuarticsieveXb5ns7.m

--- QuarticsieveXb5ns7.m
+++ QuarticsieveXb5ns7.m
@@ -2,49 +2,26 @@
 //MORDELL--WEIL SIEVE FOR QUARTIC POINTS
 /*This sieve is implemented to work for isolated degree 4 effective divisors on a curve X and for
 points of the form Q+R, where Q is an effective degree 2 divisor on X and R is the pullback
-from a rational point on a curve C admitting a degree 2 map X --> C. It is also possible to add to the IsLonely function
+from a rational point on a curve C admitting a degree 2 map X --> C.
+It is also possible to add to the IsLonely function
 the option that the effective degree 4 divisor is a pullback from an effective degree 2 divisor on C,
 but we did not need that. */
 
-/* INPUT: model for projective curve X/\Q; set L of degree 4 divisors on X,
- set divs of degree 0 divisors that generate a finite index subgroup G of J_X(Q), 
- an integer I such that I*J_X(Q) subset G,
-an abstract abelian group A isomorphic to G such that A.i corresponds to divs[i], 
-a degree 4 divisor bp on X, to be used to embed X^{(4)} in J_X,
-an automorphism w5 on X such that we do Chabauty
-relative to the quotient of X by this autmorphism,
-a subgroup B0 of A embedded via iA0: B0 --> A and a set W0 of B-coset representatives in A.
-The triple (B0,iA0,W0) is needed to start the sieve. When assuming nothing, these should be
-B0=A, iA0=Id and W0={0}. */
+/* INPUT: 
+- a model for projective curve X/\Q,
+- a set L of degree 4 divisors on X,
+- a set divs of degree 0 divisors that generate a finite index subgroup G of J_X(Q), 
+- an integer I such that I*J_X(Q) subset G,
+- an abstract abelian group A isomorphic to G such that A.i corresponds to divs[i], 
+- a degree 4 divisor bp on X, to be used for the map X^{(4)} --> J_X,
+- an automorphism w5 on X such that we do Chabauty relative to X --> X/w_5,
+- a subgroup B0 of A embedded via iA0: B0 --> A and a set W0 of B-coset representatives in A.
+The triple (B0,iA0,W0) is needed to start the sieve. 
+Without prior assumptions, these should be B0=A, iA0=Id and W0={0}. */
 
-//The following function computes the expansion of a differential 
-//om around the uniformizer tQ of a point Q, up to the (n+1)th coefficient a_n.
-ExpandDifferential:=function(om,Q,tQ,n)
-assert n ge 0;
-dt:=Differential(tQ);
-f:=om/dt;
-FF:=Parent(f);
-K:=Parent(Eltseq(Q)[1]);
-XK:=ChangeRing(Curve(om),K);
-Qpt:=XK!Eltseq(Q);
-CRK<[xK]>:=CoordinateRing(AmbientSpace(XK));
-FK:=FunctionField(XK);
-f:=FK!(Evaluate(Numerator(ProjectiveFunction(f)),xK)/Evaluate(Denominator(ProjectiveFunction(f)),xK));
-tQ:=FK!(Evaluate(Numerator(ProjectiveFunction(tQ)),xK)/Evaluate(Denominator(ProjectiveFunction(tQ)),xK));
-alist:=[Evaluate(f,Qpt)];
-if n gt 0 then
-flist:=[(f-(Evaluate(f,Qpt)))/tQ];
-for i in [1..n] do
-    Append(~alist,flist[i](Qpt));
-    Append(~flist,(flist[i]-alist[i+1])/tQ);
-end for;
-end if;
-return alist;
-end function;
-
-
-//This function verifies the conditions of Theorem XXX
+//The below function verifies the conditions of the partially relative Chabauty theorem
 //Input: degree 4 divisor QQ and prime p of good reduction
+
 IsLonely:=function(QQ,p,X,w5)
 FF:=GF(p^(12)); //Fp, Fp^2, Fp^3 and Fp^4 all embed into FF.
 XFF:=ChangeRing(X,FF);
@@ -102,10 +79,7 @@ ChabautyInfo:=function(X,w5,L,p,A,divs,I,bp,B,iA,W)
 //I must satisfy I*J_X(\Q) \subset G.
 Fp:=GF(p);
 Xp:=ChangeRing(X,Fp);
-CC,phi,psi:=ClassGroup(Xp);
-Z:=FreeAbelianGroup(1);
-degr:=hom<CC->Z | [ Degree(phi(a))*Z.1 : a in OrderedGenerators(CC)]>;
-JFp:=Kernel(degr); // This is isomorphic to J_X(\F_p).
+JFp,phi,psi:=JacobianFp(Xp);
 divsp:=[reduce(X,Xp,divi) : divi in divs];
 bpp:=reduce(X,Xp,bp); //We reduce the divisors and the basepoint
 h:=hom<A -> JFp | [JFp!psi(divp) : divp in divsp]>; //The map A--> J_X(\F_p).
@@ -126,7 +100,9 @@ jposP:=[];
 
 for x in imW do //For each z such that I*z = x, we check whether z can come from a rational point. If yes for some z, we keep x. 
     z:=x@@mI;
-    if &or[Dimension(phi(z+k)+bpp) gt 0 and (not CC!(z+k) in redL2 or p in [11,13] or not IsLonely(L[Index(redL2,CC!(z+k))],p,X,w5)) : k in K] then Append(~jposP,x);
+    if &or[Dimension(phi(z+k)+bpp) gt 0 and (not z+k in redL2 
+           or p in [11,13] or not IsLonely(L[Index(redL2,z+k)],p,X,w5)) : k in K] 
+    then Append(~jposP,x);
     end if;
 end for;
 
@@ -149,7 +125,9 @@ p;
 W,B,iA:=ChabautyInfo(X,w5,L,p,A,divs,I,bp,B,iA,W);
 // Whatever Chabauty method we use, it should output a subgroup iAp: Bp \to A and
 // a set Wp of Bp-cosets containing the hypothetical unknown points.
-if W eq [] then return true; end if;
+if W eq [] then "The sieve has terminated successfully."; 
+    return true;
+end if;
 print "The number of possible cosets unknown points can land is"; #W;
 end for;
 return B,iA,W;
